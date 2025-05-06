620224559240669fd761ae6697759690c08b492b
Daniel Saz <daniel.saz.2304@gmail.com>
2025-05-06 19:38:19 +0200

    feat: EmpleadosView

--- src/View/EmpleadosView.java
+++ src/View/EmpleadosView.java
@@ -26,19 +26,17 @@ public class EmpleadosView{
         sc.nextLine();
 
         switch(opcion){
-            case 1 -> {this.buscarDNI();}
-            case 2 -> {this.modificarEmpleado();}
-            case 3 -> {this.añadirEmpleado();}
-            case 4 -> {this.eliminarEmpleado();}
-            default -> System.out.println("Opción no válida");
-
+            case 1: {this.buscarDNI();} break;
+            case 2: {this.modificarEmpleado();} break;
+            case 3: {this.añadirEmpleado();} break;
+            case 4: {this.eliminarEmpleado();} break;
+            default: System.out.println("Opción no válida"); break;
         }
-
     }
 
     public void buscarDNI(){
         
-        System.out.println("Ingrese el DNI del empleado a buscar:");
+        System.out.println("DNI del empleado a buscar: ");
         String dni = sc.nextLine();
         empleado = empleadosDAO.buscarPorDni(dni);
 
@@ -52,21 +50,21 @@ public class EmpleadosView{
             System.out.println("Email: " + empleado.getEmail());
             System.out.println("Puesto: " + empleado.getPuesto());
             System.out.println("Jornada: " + empleado.getJornada());
-            System.out.println("Horas Extras: " + empleado.getHorasExtras());   
+            System.out.println("Horas Extras: " + empleado.getHorasExtra());   
         }else{
             System.out.println("No se ha encontrado ningun empleado con ese DNI");
         }
     }
 
     public void modificarEmpleado(){
-        System.out.println("DNI del empleado a modificar:");
+        System.out.println("DNI del empleado a modificar: ");
         String dni = sc.nextLine();
 
         empleado = empleadosDAO.buscarPorDni(dni);
         if (empleado == null) {
             System.out.println("Empleado no encontrado.");
             return;
-        }
+            }
 
             System.out.println("Datos actuales del empleado: ");
             System.out.println(empleado);
@@ -79,14 +77,106 @@ public class EmpleadosView{
             System.out.println("5.Puesto");
             System.out.println("6.Jornada");
             System.out.println("7.Horas Extras");
-            
 
+            int opcion = sc.nextInt();
+            sc.nextLine();
+            String atributo ="";
+            String valor ="";
+
+            switch(opcion){
+                case 1: {
+                    atributo = "Nombre";
+                    System.out.println("Nuevo nombre: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 2: {
+                    atributo = "Apellido";
+                    System.out.println("Nuevo apellido: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 3: {
+                    atributo = "Telefono";
+                    System.out.println("Nuevo telefono: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 4: {
+                    atributo = "Email";
+                    System.out.println("Nuevo email: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 5: {
+                    atributo = "Puesto";
+                    System.out.println("Nuevo puesto: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 6: {
+                    atributo = "Jornada";
+                    System.out.println("Nueva jornada: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                case 7: {
+                    atributo = "Horas Extras";
+                    System.out.println("Nuevas horas extras: ");
+                    valor = sc.nextLine();
+                    break;
+                }
+                default: {
+                    System.out.println("Opcion no valida");
+                    return;
+                }
+            }
+            empleadosDAO.actualizar(atributo, valor, dni);
+            System.out.println("Empleado modificado");
     }
 
     public void añadirEmpleado(){
+
+        System.out.println("Añadir un nuevo empleado");
+
+        System.out.println("DNI: ");
+        String dni = sc.nextLine();
+        System.out.println("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Apellidos: ");
+        String apellido = sc.nextLine();
+        System.out.println("Telefono: ");
+        String telefono = sc.nextLine();
+        System.out.println("Email: ");
+        String email = sc.nextLine();
+        System.out.println("Puesto: ");
+        String puesto = sc.nextLine();
+        System.out.println("Jornada: ");
+        String jornada = sc.nextLine();
+        System.out.println("Horas Extras: ");
+        int horasExtras = sc.nextInt();
+        sc.nextLine();
+
+        Empleados nuevoEmpleado = new Empleados(dni, nombre, apellido, puesto, email, telefono, jornada, horasExtras);
+        empleadosDAO.insertar(nuevoEmpleado);
+
+        System.out.println("Nuevo empleado añadido.");
     }
 
     public void eliminarEmpleado(){
+
+        System.out.println("Eliminar emplado.");
+        System.out.println("DNI del empleado a eliminar: ");
+        String dni = sc.nextLine();
+
+        System.out.println("¿Seguro que quieres eliminar este empleado (Si/No)?");
+        String respuesta = sc.nextLine();
+        if (respuesta.equalsIgnoreCase("Si")) {
+            empleadosDAO.eliminar(dni);
+            System.out.println("Empleado eliminado.");
+        }else{
+            System.out.println("Operacion cancelada");
+        }
     }
 
 }
