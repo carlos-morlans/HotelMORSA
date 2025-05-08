76abb478ca378f2cef7ed9b457cc8eb8f907e378
Marcos A <marcosalegredrop@gmail.com>
2025-05-08 08:56:36 +0200

    Reservas y pagos DAO y VIEW

---
+++ src/Dao/PagosDAO.java
@@ -0,0 +1,29 @@
+package Dao;
+
+import java.time.LocalDate;
+
+public class PagosDAO {
+
+    public void filtroCuantia(int limite, Double cuantiaMaxima, Double cuantiaMinima, String tipo) {
+       
+    }
+
+    public void filtroDNIEmpleado(int limite, String dniEmpleado, String tipo) {
+       
+    }
+
+    public void filtroIDReserva(int limite, int idReserva, String tipo) {
+       
+    }
+
+    public void filtroRecientes(int limite, String tipo) {
+       
+    }
+
+    public void filtroFecha(int limite, LocalDate fechaInicio, LocalDate fechaFin, String tipo) {
+       
+    }
+
+
+
+}
